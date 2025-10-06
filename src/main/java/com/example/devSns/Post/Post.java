41a6b2e58c93f8d11dfcb45bb4576d415e7ed7c6
tomchccom <dreamkms2014@gmail.com>
2025-10-06 10:47:23 +0900

    fix(entity) : JPA 어노테이션 제거

--- src/main/java/com/example/devSns/Post/Post.java
+++ src/main/java/com/example/devSns/Post/Post.java
@@ -1,21 +1,17 @@
 package com.example.devSns.Post;
 
-
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Entity
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class Post {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String content;
@@ -23,16 +19,6 @@ public class Post {
     private String username;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
 
 
 }
