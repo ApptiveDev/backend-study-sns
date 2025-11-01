e3f27b2ca3335ab2756ada18ee92513da1fd282a
tomchccom <dreamkms2014@gmail.com>
2025-11-01 21:13:22 +0900

    feat: 댓글 엔티티 추가

---
+++ src/main/java/com/example/devSns/Comment/Comment.java
@@ -0,0 +1,33 @@
+package com.example.devSns.Comment;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
