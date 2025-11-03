840c4196256907228009abb60b9001e8c6b1fe00
tomchccom <dreamkms2014@gmail.com>
2025-11-03 16:51:13 +0900

    feat: 엔티티, 컨트롤러, 서비스, 리포지터리 생성

---
+++ src/main/java/com/example/devSns/Comment/CommentController.java
@@ -0,0 +1,7 @@
+package com.example.devSns.Comment;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommentController {
+}
---
+++ src/main/java/com/example/devSns/Comment/CommentService.java
@@ -0,0 +1,16 @@
+package com.example.devSns.Comment;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+
+    // 댓글 달기. 특정 게시글 밑에 작성 , 게시글 상세 조회 후 작성이 가능하도록
+    // 예상 URL posts/1/comments
+    // 댓글 수정
+    // 댓글 목록 조회 - 게시글 밑에 나타나게끔. 그렇다면
+    // 댓글 조회 - 특정 게시글 조회 시 댓글도 조회되게끔
+    // 댓글 삭제
+
+}
